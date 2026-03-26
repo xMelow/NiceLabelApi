29cdbc3fb896e02577479dcd5b0d3e6a2878b34f
Flor Stellamans <florstellamamans@gmail.com>
2026-03-26 12:37:16 +0100

    initial commit

---
+++ NiceLabelApi/Controllers/NiceLabelController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace NiceLabelApi.Controllers
+{
+    [RoutePrefix("nicelabel")]
+    public class NiceLabelController : ApiController
+    {
+        [HttpGet]
+        [Route("test")]
+        public IHttpActionResult Test()
+        {
+            return Ok("NiceLabel API is running!");
+        }
+    }
+}
